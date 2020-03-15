bcad666a8f879bc712e65005ba4e0140efa181c4
Splerge <damnsplurge@gmail.com>
2020-03-15 16:54:40 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://spencerjp.github.io/spence-tm-discovery/precache-manifest.277de38c21829ba5fdd6289a58bb82c7.js"
+  "/precache-manifest.1313dd415b2353826ef26863000c97a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://spencerjp.github.io/spence-tm-discovery/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
