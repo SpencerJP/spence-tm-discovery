9f8d3e5531cb80cf5cef5f936d3ee7a38d5241ec
Splerge <damnsplurge@gmail.com>
2020-03-15 15:46:00 +1000

    fixed github pages url issue

--- src/Components/EventSiteRoutes/EventSiteRoutes.jsx
+++ src/Components/EventSiteRoutes/EventSiteRoutes.jsx
@@ -3,16 +3,21 @@ import { Switch, Route } from "react-router-dom"
 import { EventList } from "../EventsListComponents"
 import { About } from "../AboutUs"
 
+let prefix = window.REACT_APP_URL_PREFIX
+if (prefix === "%REACT_APP_URL_PREFIX%") {
+	prefix = ""
+}
+
 export function EventSiteRoutes() {
 	return (
 		<Switch>
-			<Route exact path={`${window.REACT_APP_URL_SUFFIX}/`}>
+			<Route exact path={`${prefix}/`}>
 				<EventList />
 			</Route>
-			<Route exact path={`${window.REACT_APP_URL_SUFFIX}/home`}>
+			<Route exact path={`${prefix}/home`}>
 				<EventList />
 			</Route>
-			<Route exact path={`${window.REACT_APP_URL_SUFFIX}/about`}>
+			<Route exact path={`${prefix}/about`}>
 				<About />
 			</Route>
 		</Switch>
